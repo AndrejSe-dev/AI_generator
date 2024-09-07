3162f3e8f1fbc9faa32fff2356711b93ce421103
Andrej Sekrerak <annndreeejjj@gmail.com>
2024-09-07 21:46:45 +0200

    Hotovo

--- check_path.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Skontrolujte, či priečinok existuje
-model_dir = "fine_tuned_distilgpt2"
-if os.path.exists(model_dir):
-    print(f"Model uložený v priečinku: {os.path.abspath(model_dir)}")
-else:
-    print("Priečinok s modelom neexistuje.")
